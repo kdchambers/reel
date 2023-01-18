4b40a7a7c905386bd0ec985860d36a8c2a072eb1
kdchambers <kd.chambers97@gmail.com>
2023-01-18 16:07:55 -0500

    add more dbus bindings

--- src/dbus_bindings.zig
+++ src/dbus_bindings.zig
@@ -2,26 +2,44 @@
 // Copyright (c) 2023 Keith Chambers
 
 extern fn dbus_bus_get(bus_type: dbus.BusType, err: *dbus.Error) *dbus.Connection;
+extern fn dbus_bus_add_match(connection: *dbus.Connection, rule: [*:0]const u8, err: ?*dbus.Error) void;
+
 extern fn dbus_error_init(err: *dbus.Error) void;
 extern fn dbus_error_is_set(err: *const dbus.Error) i32;
+
+extern fn dbus_message_append_args(message: *dbus.Message, first_arg_type: i32, ...) i32;
+extern fn dbus_message_unref(message: *dbus.Message) void;
+extern fn dbus_message_iter_init(message: *dbus.Message, iter: *dbus.MessageIter) i32;
+extern fn dbus_message_iter_get_arg_type(iter: *dbus.MessageIter) i32;
+extern fn dbus_message_iter_recurse(iter: *dbus.MessageIter, sub_iter: *dbus.MessageIter) void;
+extern fn dbus_message_iter_get_basic(iter: *dbus.MessageIter, value: *void) void;
+extern fn dbus_message_iter_init_append(query_message: *dbus.Message, iter: *dbus.MessageIter) void;
+extern fn dbus_message_iter_append_basic(iter: *dbus.MessageIter, value_type: i32, value: *const void) dbus.bool_t;
+extern fn dbus_message_iter_close_container(parent_iter: *dbus.MessageIter, iter: *dbus.MessageIter) dbus.bool_t;
+extern fn dbus_message_is_signal(message: *dbus.Message, interface: [*:0]const u8, signal_name: [*:0]const u8) dbus.bool_t;
+extern fn dbus_message_iter_next(iter: *dbus.MessageIter) dbus.bool_t;
+extern fn dbus_message_iter_open_container(
+    iter: *dbus.MessageIter,
+    container_type: i32,
+    signature: ?[*:0]const u8,
+    sub_iter: *dbus.MessageIter,
+) dbus.bool_t;
 extern fn dbus_message_new_method_call(
     destination: [*:0]const u8,
     path: [*:0]const u8,
     interface: [*:0]const u8,
     method: [*:0]const u8,
 ) *dbus.Message;
-extern fn dbus_message_append_args(message: *dbus.Message, first_arg_type: i32, ...) i32;
+
+extern fn dbus_connection_flush(connection: *dbus.Connection) void;
+extern fn dbus_connection_read_write(connection: *dbus.Connection, timeout_ms: i32) dbus.bool_t;
+extern fn dbus_connection_pop_message(connection: *dbus.Connection) ?*dbus.Message;
 extern fn dbus_connection_send_with_reply_and_block(
     connection: *dbus.Connection,
     message: *dbus.Message,
     timeout_ms: i32,
     err: *dbus.Error,
 ) *dbus.Message;
-extern fn dbus_message_unref(message: *dbus.Message) void;
-extern fn dbus_message_iter_init(message: *dbus.Message, iter: *dbus.MessageIter) i32;
-extern fn dbus_message_iter_get_arg_type(iter: *dbus.MessageIter) i32;
-extern fn dbus_message_iter_recurse(iter: *dbus.MessageIter, sub_iter: *dbus.MessageIter) void;
-extern fn dbus_message_iter_get_basic(iter: *dbus.MessageIter, value: *void) void;
 
 const dbus = struct {
     const Error = extern struct {
