8af10b3f2a33964015d84c7223fa52d19da564e4
kdchambers <kd.chambers97@gmail.com>
2023-01-28 09:10:56 -0500

    add more definitions to libav.zig

--- src/libav.zig
+++ src/libav.zig
@@ -14,11 +14,14 @@ const libav = @cImport({
     @cInclude("libavfilter/buffersrc.h");
 });
 
-const EAGAIN: i32 = -11;
-const EINVAL: i32 = -22;
+pub const EAGAIN: i32 = -11;
+pub const EINVAL: i32 = -22;
+
+pub const LOG_DEBUG = libav.AV_LOG_DEBUG;
 
 pub const CodecContext = libav.AVCodecContext;
 pub const FormatContext = libav.AVFormatContext;
+pub const OutputFormat = libav.AVOutputFormat;
 pub const Frame = libav.AVFrame;
 pub const Stream = libav.AVStream;
 pub const Packet = libav.AVPacket;
@@ -26,16 +29,25 @@ pub const InputFormat = libav.AVInputFormat;
 pub const MediaType = libav.AVMediaType;
 pub const Dictionary = libav.AVDictionary;
 pub const Codec = libav.AVCodec;
-pub const CodecID = libav.AVCodecID;
+pub const FilterGraph = libav.AVFilterGraph;
 
-extern fn av_dict_set(dict: *?*Dictionary, key: [*:0]const u8, value: [*:0]const u8, flags: i32) callconv(.C) i32;
-extern fn av_dict_free(dict: **Dictionary) callconv(.C) void;
-extern fn av_frame_alloc() callconv(.C) *Frame;
-extern fn av_frame_unref(frame: *Frame) callconv(.C) void;
-extern fn av_log_set_level(level: i32) callconv(.C) void;
-extern fn av_find_input_format(short_name: [*:0]const u8) callconv(.C) ?*InputFormat;
+pub extern fn av_dict_set(dict: *?*Dictionary, key: [*:0]const u8, value: [*:0]const u8, flags: i32) callconv(.C) i32;
+pub extern fn av_dict_free(dict: **Dictionary) callconv(.C) void;
+pub extern fn av_frame_alloc() callconv(.C) *Frame;
+pub extern fn av_frame_unref(frame: *Frame) callconv(.C) void;
+pub extern fn av_log_set_level(level: i32) callconv(.C) void;
+pub extern fn av_find_input_format(short_name: [*:0]const u8) callconv(.C) ?*InputFormat;
+pub extern fn av_guess_format(short_name: ?[*:0]const u8, filename: [*:0]const u8, mime_type: ?[*:0]const u8) ?*OutputFormat;
+pub extern fn avcodec_find_encoder(codec_id: CodecID) ?*Codec;
 
-extern fn av_find_best_stream(
+pub extern fn avformat_alloc_output_context2(
+    context: *?*FormatContext,
+    oformat: ?*OutputFormat,
+    format_name: ?[*:0]const u8,
+    filename: ?[*:0]const u8,
+) callconv(.C) i32;
+
+pub extern fn av_find_best_stream(
     ic: *FormatContext,
     media_type: MediaType,
     wanted_stream: i32,
@@ -44,14 +56,14 @@ extern fn av_find_best_stream(
     flags: i32,
 ) callconv(.C) i32;
 
-extern fn av_dump_format(
+pub extern fn av_dump_format(
     format_context: *FormatContext,
     index: i32,
     url: [*:0]const u8,
     is_output: i32,
 ) callconv(.C) void;
 
-extern fn av_image_alloc(
+pub extern fn av_image_alloc(
     pointers: [*][*]u8,
     linesizes: *[4]i32,
     width: i32,
@@ -60,21 +72,27 @@ extern fn av_image_alloc(
     alignment: i32,
 ) callconv(.C) i32;
 
-extern fn avcodec_find_decoder(id: CodecID) callconv(.C) *Codec;
-extern fn avcodec_open2(context: *CodecContext, codec: *const Codec, options: *?*Dictionary) callconv(.C) i32;
-extern fn avcodec_send_packet(context: *CodecContext, packet: *const Packet) callconv(.C) i32;
-extern fn avcodec_receive_frame(context: *CodecContext, frame: *Frame) callconv(.C) i32;
+pub extern fn avcodec_find_decoder(id: CodecID) callconv(.C) *Codec;
+pub extern fn avcodec_open2(context: *CodecContext, codec: *const Codec, options: *?*Dictionary) callconv(.C) i32;
+pub extern fn avcodec_send_packet(context: *CodecContext, packet: *const Packet) callconv(.C) i32;
+pub extern fn avcodec_receive_frame(context: *CodecContext, frame: *Frame) callconv(.C) i32;
+pub extern fn avcodec_alloc_context3(codec: ?*Codec) ?*CodecContext;
 
-extern fn avdevice_register_all() callconv(.C) void;
+pub extern fn avdevice_register_all() callconv(.C) void;
 
-extern fn avformat_alloc_context() callconv(.C) ?*FormatContext;
-extern fn avformat_open_input(
+pub extern fn avformat_alloc_context() callconv(.C) ?*FormatContext;
+pub extern fn avformat_open_input(
     format_context: **FormatContext,
     input_path: [*:0]const u8,
     input_format: *InputFormat,
     options: *?*Dictionary,
 ) callconv(.C) i32;
-extern fn avformat_find_stream_info(format_context: *FormatContext, options: ?*?*Dictionary) callconv(.C) i32;
+pub extern fn avformat_find_stream_info(format_context: *FormatContext, options: ?*?*Dictionary) callconv(.C) i32;
+pub extern fn avformat_new_stream(stream_context: *FormatContext, codec: *Codec) ?*Stream;
+
+pub const guessFormat = av_guess_format;
+pub const formatAllocOutputContext2 = avformat_alloc_output_context2;
+pub const codecFindEncoder = avcodec_find_encoder;
 
 pub const dictSet = av_dict_set;
 pub const dictFree = av_dict_free;
@@ -91,11 +109,106 @@ pub const deviceRegisterAll = avdevice_register_all;
 pub const formatFindStreamInfo = avformat_find_stream_info;
 pub const formatOpenInput = avformat_open_input;
 pub const formatAllocContext = avformat_alloc_context;
+pub const formatNewStream = avformat_new_stream;
 
 pub const codecReceiveFrame = avcodec_receive_frame;
 pub const codecSendPacket = avcodec_send_packet;
 pub const codecOpen2 = avcodec_open2;
 pub const codecFindDecoder = avcodec_find_decoder;
+pub const codecAllocContext3 = avcodec_alloc_context3;
+
+pub const ColorRange = enum(u32) {
+    unspecified = 0,
+    mpeg = 1,
+    jpeg = 2,
+};
+
+pub const CodecID = enum(i32) {
+    none = 0,
+    MPEG1VIDEO,
+    MPEG2VIDEO,
+    H261,
+    H263,
+    RV10,
+    RV20,
+    MJPEG,
+    MJPEGB,
+    LJPEG,
+    SP5X,
+    JPEGLS,
+    MPEG4,
+    RAWVIDEO,
+    MSMPEG4V1,
+    MSMPEG4V2,
+    MSMPEG4V3,
+    WMV1,
+    WMV2,
+    H263P,
+    H263I,
+    FLV1,
+    SVQ1,
+    SVQ3,
+    DVVIDEO,
+    HUFFYUV,
+    CYUV,
+    h264,
+    INDEO3,
+    VP3,
+    THEORA,
+    ASV1,
+    ASV2,
+    FFV1,
+    @"4XM",
+    VCR1,
+    CLJR,
+    MDEC,
+    ROQ,
+    INTERPLAY_VIDEO,
+    XAN_WC3,
+    XAN_WC4,
+    RPZA,
+    CINEPAK,
+    WS_VQA,
+    MSRLE,
+    MSVIDEO1,
+    IDCIN,
+    @"8BPS",
+    SMC,
+    FLIC,
+    TRUEMOTION1,
+    VMDVIDEO,
+    MSZH,
+    ZLIB,
+    QTRLE,
+    TSCC,
+    ULTI,
+    QDRAW,
+    VIXL,
+    QPEG,
+    PNG,
+    PPM,
+    PBM,
+    PGM,
+    PGMYUV,
+    PAM,
+    FFVHUFF,
+    RV30,
+    RV40,
+    VC1,
+    WMV3,
+    LOCO,
+    WNV1,
+    AASC,
+    INDEO2,
+    FRAPS,
+    TRUEMOTION2,
+    BMP,
+    CSCD,
+    MMVIDEO,
+    ZMBV,
+    AVS,
+    SMACKVIDEO,
+};
 
 pub const PixelFormat = enum(i32) {
     YUV420P = 0,
