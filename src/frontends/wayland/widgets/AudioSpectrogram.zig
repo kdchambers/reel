d3ec9ae28c68f72245af728ade226d783fd9ab06
kdchambers <kd.chambers97@gmail.com>
2023-04-19 12:16:36 -0500

    ui: AudioSpectrogram lerps between from and to colors
    The color_to (Color of top of bar) is calculated based on the bars height
    
    Previously, regardless of the height of the bar it was set to `color_to`

--- src/frontends/wayland/widgets/AudioSpectrogram.zig
+++ src/frontends/wayland/widgets/AudioSpectrogram.zig
@@ -4,6 +4,7 @@
 // TODO: This is just a bar graph..
 
 const std = @import("std");
+const assert = std.debug.assert;
 const geometry = @import("../../../geometry.zig");
 const Extent2D = geometry.Extent2D;
 const ScaleFactor2D = geometry.ScaleFactor2D;
@@ -14,6 +15,9 @@ const Vertex = graphics.GenericVertex;
 const FaceWriter = graphics.FaceWriter;
 const QuadFace = graphics.QuadFace;
 const RGBA = graphics.RGBA;
+
+const bar_color_from = graphics.RGBA(f32).fromInt(50, 100, 65, 255);
+const bar_color_to = graphics.RGBA(f32).fromInt(250, 0, 0, 255);
 
 // TODO: Remove
 const root = @import("base.zig");
@@ -35,40 +39,70 @@ pub fn draw(
 
     const bar_width: f32 = extent.width / (@intToFloat(f32, self.bin_count) * 2.0);
     const bar_spacing = (extent.width - (bar_width * @intToFloat(f32, self.bin_count))) / @intToFloat(f32, self.bin_count + 1);
-
     const bar_increment: f32 = bar_width + bar_spacing;
-    const bar_color = graphics.RGBA(f32).fromInt(50, 100, 65, 255);
+
+    const volume_range_db: f32 = self.max_cutoff_db - self.min_cutoff_db;
+    assert(volume_range_db >= 0);
 
     self.vertex_index = root.face_writer_ref.vertices_used;
     var quads = try root.face_writer_ref.allocate(QuadFace, self.bin_count);
     for (freq_bins, quads, 0..) |freq_value, *quad, i| {
         const db_clamped = @min(self.max_cutoff_db, @max(self.min_cutoff_db, freq_value));
-        const bar_height = extent.height - ((db_clamped / self.min_cutoff_db) * extent.height);
+        const percentage = (db_clamped + (-self.min_cutoff_db)) / volume_range_db;
+        assert(percentage >= 0.0);
+        assert(percentage <= 1.0);
+
+        const bar_height = percentage * extent.height;
         const bar_extent = Extent2D(f32){
             .x = extent.x + (@intToFloat(f32, i) * bar_increment),
             .y = extent.y,
             .width = bar_width,
             .height = bar_height,
         };
-        quad.* = graphics.quadColored(bar_extent, bar_color, .bottom_left);
-        quad.*[0].color = graphics.RGBA(f32).fromInt(150, 50, 70, 255);
-        quad.*[1].color = graphics.RGBA(f32).fromInt(150, 50, 70, 255);
+        quad.* = graphics.quadColored(bar_extent, bar_color_from, .bottom_left);
+        const bar_color_top = graphics.RGBA(f32){
+            .r = lerp(bar_color_from.r, bar_color_to.r, percentage),
+            .g = lerp(bar_color_from.g, bar_color_to.g, percentage),
+            .b = lerp(bar_color_from.b, bar_color_to.b, percentage),
+            .a = 1.0,
+        };
+        quad.*[0].color = bar_color_top;
+        quad.*[1].color = bar_color_top;
     }
 }
 
 pub fn update(self: *@This(), freq_bins: []f32, screen_scale: ScaleFactor2D(f32)) !void {
-    std.debug.assert(freq_bins.len == self.bin_count);
+    assert(freq_bins.len == self.bin_count);
     var quads = @ptrCast([*]QuadFace, &root.vertices_buffer_ref[self.vertex_index]);
     const height = self.height_pixels * screen_scale.vertical;
     var i: usize = 0;
     const y_lower = quads[i][2].y;
-    std.debug.assert(y_lower == quads[i][3].y);
+    assert(y_lower == quads[i][3].y);
+
+    const volume_range_db: f32 = self.max_cutoff_db - self.min_cutoff_db;
+    assert(volume_range_db >= 0);
+
     while (i < self.bin_count) : (i += 1) {
         const freq_value = freq_bins[i];
         const db_clamped = @min(self.max_cutoff_db, @max(self.min_cutoff_db, freq_value));
-        const bar_height = height - ((db_clamped / self.min_cutoff_db) * height);
+        const percentage = (db_clamped + (-self.min_cutoff_db)) / volume_range_db;
+        assert(percentage >= 0.0);
+        assert(percentage <= 1.0);
+        const bar_color_top = graphics.RGBA(f32){
+            .r = lerp(bar_color_from.r, bar_color_to.r, percentage),
+            .g = lerp(bar_color_from.g, bar_color_to.g, percentage),
+            .b = lerp(bar_color_from.b, bar_color_to.b, percentage),
+            .a = 1.0,
+        };
+        const bar_height = percentage * height;
         std.debug.assert(quads[i][0].y == quads[i][1].y);
         quads[i][0].y = y_lower - bar_height;
         quads[i][1].y = y_lower - bar_height;
+        quads[i][0].color = bar_color_top;
+        quads[i][1].color = bar_color_top;
     }
+}
+
+fn lerp(from: f32, to: f32, value: f32) f32 {
+    return from + (value * (to - from));
 }
