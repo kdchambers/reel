82a2c964adb141b7e27b909e47d837e9e7b90ba9
kdchambers <kd.chambers97@gmail.com>
2023-05-13 21:03:54 -0500

    added generic encoder / decoder to utils

--- src/utils.zig
+++ src/utils.zig
@@ -6,6 +6,7 @@ pub const Timer = @import("utils/Timer.zig");
 pub const FixedBuffer = @import("utils/FixedBuffer.zig").FixedBuffer;
 
 const std = @import("std");
+const assert = std.debug.assert;
 const c = @cImport({
     @cInclude("time.h");
 });
@@ -57,4 +58,99 @@ test "Duration" {
     try expect(duration.minutes == 25);
     try expect(duration.seconds == 33);
     try expect(duration.milliseconds == 345);
+}
+
+pub fn Encoder(comptime Encoding: type, comptime buffer_size: comptime_int) type {
+    return struct {
+        pub const Decoder = struct {
+            buffer: []u8,
+            index: usize,
+
+            pub fn next(self: *@This()) ?Encoding {
+                if (self.index == self.buffer.len)
+                    return null;
+                assert(self.index < self.buffer.len);
+                defer self.index += 1;
+                return @intToEnum(Encoding, self.buffer[self.index]);
+            }
+
+            pub fn readString(self: *@This()) []const u8 {
+                const len = self.readInt(u16);
+                const index_start = self.index;
+                const index_end = index_start + len;
+                self.index += len;
+                return self.buffer[index_start..index_end];
+            }
+
+            pub fn readInt(self: *@This(), comptime T: type) !T {
+                const alignment = @alignOf(T);
+                const misaligment = self.index % alignment;
+                if (misaligment > 0) {
+                    std.debug.assert(misaligment < alignment);
+                    const padding_required = alignment - misaligment;
+                    std.debug.assert(padding_required < alignment);
+                    self.index += padding_required;
+                    std.debug.assert(self.index % alignment == 0);
+                }
+
+                const bytes_to_read = @sizeOf(T);
+                if (self.index + bytes_to_read > self.buffer.len)
+                    return error.EndOfBuffer;
+                defer self.index += bytes_to_read;
+                return @ptrCast(*T, @alignCast(alignment, &self.buffer[self.index])).*;
+            }
+        };
+
+        buffer: [buffer_size]u8 = undefined,
+        used: usize = 0,
+
+        pub inline fn reset(self: *@This()) void {
+            self.used = 0;
+        }
+
+        pub fn write(self: *@This(), request: Encoding) !void {
+            if (self.used == self.buffer.len)
+                return error.EndOfBuffer;
+            assert(self.used < self.buffer.len);
+            self.buffer[self.used] = @enumToInt(request);
+            self.used += 1;
+        }
+
+        pub fn writeString(self: *@This(), bytes: []const u8) !void {
+            if ((self.used + bytes.len + @sizeOf(u16)) >= self.buffer.len)
+                return error.EndOfBuffer;
+            self.writeInt(u16, @intCast(u16, bytes.len)) catch unreachable;
+            var i: usize = self.used;
+            for (bytes) |char| {
+                self.buffer[i] = char;
+                i += 1;
+            }
+            self.used += bytes.len;
+        }
+
+        pub fn writeInt(self: *@This(), comptime T: type, value: T) !void {
+            const alignment = @alignOf(T);
+            const misaligment = self.used % alignment;
+            if (misaligment > 0) {
+                assert(misaligment < alignment);
+                const padding_required = alignment - misaligment;
+                assert(padding_required < alignment);
+                self.used += padding_required;
+                assert(self.used % alignment == 0);
+            }
+
+            const bytes_to_read = @sizeOf(T);
+            if (self.used + bytes_to_read > self.buffer.len)
+                return error.EndOfBuffer;
+            @ptrCast(*T, @alignCast(alignment, &self.buffer[self.used])).* = value;
+            self.used += bytes_to_read;
+        }
+
+        pub fn toDecoder(self: *@This()) Decoder {
+            return .{
+                .buffer = self.buffer[0..self.used],
+                .index = 0,
+            };
+        }
+    };
 }
