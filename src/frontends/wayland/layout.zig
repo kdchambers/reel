cc52ae5457c5a11fd1d916991b5b3fea7cd5b804
kdchambers <kd.chambers97@gmail.com>
2023-05-30 04:41:50 -0500

    wayland: Add Placement type to layout

--- src/frontends/wayland/layout.zig
+++ src/frontends/wayland/layout.zig
@@ -37,6 +37,42 @@ pub const Margins = struct {
     bottom: f32 = 0,
 };
 
+pub const Placement = struct {
+    anchor: Anchors = .{},
+    margin: Margins = .{},
+    z: f32 = ui_layer.middle,
+
+    pub inline fn top(self: @This()) f32 {
+        const base = self.anchor.top orelse self.anchor.bottom.? - self.margin.bottom;
+        return base + self.margin.top;
+    }
+
+    pub inline fn bottom(self: @This()) f32 {
+        const base = self.anchor.bottom orelse self.anchor.top.? + self.margin.top;
+        return base - self.margin.bottom;
+    }
+
+    pub inline fn left(self: @This()) f32 {
+        const base = self.anchor.left orelse self.anchor.right.? - self.margin.right;
+        return base + self.margin.left;
+    }
+
+    pub inline fn right(self: @This()) f32 {
+        const base = self.anchor.right orelse self.anchor.left.? + self.margin.left;
+        return base - self.margin.right;
+    }
+
+    pub fn placement(self: @This()) Coordinates3D(f32) {
+        const base_x = self.anchor.left orelse self.anchor.right.? - self.margin.right;
+        const base_y = self.anchor.bottom orelse self.anchor.top.? + self.margin.top;
+        return .{
+            .x = base_x + self.margin.left,
+            .y = base_y - self.margin.bottom,
+            .z = self.z,
+        };
+    }
+};
+
 pub const Region = struct {
     anchor: Anchors = .{},
     margin: Margins = .{},
